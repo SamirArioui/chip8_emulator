07386e0bdd44e60f02825506cde64cd2f0cd0248
sarioui <arioui.samir@gmail.com>
2024-01-12 20:01:23 +0100

    test: test read instructions from chip8 program

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::fs;
 
 fn main() {
     let program = fs::read("data/TETRIS").expect("Failed to read file");
-    let mut ram = Ram::new();
-    ram.write_byte(123, 123);
-    println!("{}", ram.read_byte(123));
+    let address = 0x3EA - 0x200;
+    let inst_p1 = program[address];
+    let inst_p2 = program[address + 1];
+    println!("{:#X}-{:#X}", inst_p1, inst_p2);
 }
