9c181d9ab7ae45e9e4592e33f44801e6b7fad4be
SamirArioui <arioui.samir@gmail.com>
2024-01-27 23:23:45 +0100

    feat: implementation of few instructions

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,3 +1,4 @@
+use rand::random;
 use std::fs;
 
 const RAM_SIZE: usize = 4096;
@@ -101,6 +102,7 @@ impl Chip8 {
         default_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
         default_emu
     }
+
     fn fetch(&mut self) -> u16 {
         let higher_byte = self.ram[self.pc as usize] as u16;
         let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
@@ -118,9 +120,31 @@ impl Chip8 {
         match (d1, d2, d3, d4) {
             (0, 0, 0, 0) => (),
             (0, 0, 0xe, 0) => self.clr(),
+            (0, 0, 0xe, 0xe) => self.ret(),
+            (0x1, 0, 0, 0) => self.jump(opcode),
+            (0x2, 0, 0, 0) => self.call(opcode),
+            (0x3, 0, 0, 0) => self.se_vx_byte(opcode),
+            (0x4, 0, 0, 0) => self.sne_vx_byte(opcode),
+            (0x5, 0, 0, 0) => self.se_vx_vy(opcode),
+            (0x6, 0, 0, 0) => self.ld_vx_byte(opcode),
+            (0x7, 0, 0, 0) => self.add_vx_byte(opcode),
+            (0x8, 0, 0, 0) => self.ld_vx_vy(opcode),
+            (0x8, 0, 0, 1) => self.or_vx_vy(opcode),
+            (0x8, 0, 0, 2) => self.and_vx_vy(opcode),
+            (0x8, 0, 0, 3) => self.xor_vx_vy(opcode),
+            (0x8, 0, 0, 4) => self.add_vx_vy(opcode),
+            (0x8, 0, 0, 5) => self.sub_vx_vy(opcode),
+            (0x8, 0, 0, 6) => self.shr_vx_vy(opcode),
+            (0x8, 0, 0, 7) => self.subn_vx_vy(opcode),
+            (0x8, 0, 0, 0xe) => self.shl_vx_vy(opcode),
+            (0x9, 0, 0, 0) => self.sne_vx_vy(opcode),
+            (0xa, 0, 0, 0) => self.ld_i(opcode),
+            (0xb, 0, 0, 0) => self.jump_v0(opcode),
+            (0xc, 0, 0, 0) => self.rnd_vx(opcode),
             _ => unimplemented!("Unimplemented opcode: {:X}", opcode),
         }
     }
+
     fn push(&mut self, val: u16) {
         self.stack[self.sp as usize] = val;
         self.sp += 1;
@@ -131,59 +155,144 @@ impl Chip8 {
         self.stack[self.sp as usize]
     }
 
-    fn load_program(&mut self, path: String) {
-        let program = fs::read(path).expect("Failed to read file");
-        for (i, item) in program.iter().enumerate() {
-            self.ram[i + self.pc] = *item;
-        }
-    }
-
-    fn get_curr_opcode(&mut self) -> u16 {
-        let op1: u16 = (self.ram[self.pc] as u16) << 8;
-        let op2: u16 = self.ram[self.pc + 1] as u16;
-        op1 | op2
-    }
-
-    fn ld_i(&mut self, opcode: u16) {
-        self.i_reg = opcode & 0x0FFF;
-        self.pc += 2;
-    }
-
-    fn call(&mut self, opcode: u16) {
-        self.sp += 1;
-        self.stack[self.sp] = self.pc;
-        self.pc = (opcode & 0x0FFF) as usize;
-    }
-
-    fn ld_vx_byte(&mut self, opcode: u16) {
-        let vx = ((opcode & 0x0F00) >> 8) as usize;
-        let byte = (opcode & 0x00FF) as u8;
-        self.v_reg[vx] = byte;
-        self.pc += 2;
-    }
-
-    fn add_vx_byte(&mut self, opcode: u16) {
-        let vx = ((opcode & 0x0F00) >> 8) as usize;
-        let byte = (opcode & 0x00FF) as u8;
-        self.v_reg[vx] += byte;
-        self.pc += 2;
+    fn clr(&mut self) {
+        self.screen = [false; SCREEN_WIDTH * SCREEN_WIDTH]
     }
 
     fn ret(&mut self) {
-        self.pc = self.stack[self.sp];
-        self.sp -= 1;
-        self.pc += 2;
+        self.pc = self.pop();
     }
 
-    fn run_opcode(&mut self, opcode: u16) {
-        println!("{:X}", opcode);
-        match opcode {
-            0x2000..=0x2fff => self.call(opcode),
-            0x00ee..=0x00ee => self.ret(),
-            0x6000..=0x6fff => self.ld_vx_byte(opcode),
-            0xa000..=0xafff => self.ld_i(opcode),
-            0x7000..=0x7fff => self.add_vx_byte(opcode),
-            _ => println!("UKNOWN INSTRUCTION"),
+    fn jump(&mut self, opcode: u16) {
+        self.pc = opcode & 0x0fff
+    }
+
+    fn call(&mut self, opcode: u16) {
+        self.push(self.pc);
+        self.pc = opcode & 0x0fff;
+    }
+
+    fn se_vx_byte(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let byte = (opcode & 0x00ff) as u8;
+        if self.v_reg[vx] == byte {
+            self.pc += 2;
         }
+    }
+
+    fn sne_vx_byte(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let byte = (opcode & 0x00ff) as u8;
+        if self.v_reg[vx] != byte {
+            self.pc += 2;
+        }
+    }
+
+    fn se_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        if self.v_reg[vx] == self.v_reg[vy] {
+            self.pc += 2;
+        }
+    }
+
+    fn ld_vx_byte(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let byte = (opcode & 0x00ff) as u8;
+        self.v_reg[vx] = byte;
+    }
+
+    fn add_vx_byte(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let byte = (opcode & 0x00ff) as u8;
+        // TODO: use wrapping_add for overflow panic
+        self.v_reg[vx] += byte;
+    }
+
+    fn ld_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        self.v_reg[vx] = self.v_reg[vy];
+    }
+
+    fn or_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        self.v_reg[vx] |= self.v_reg[vy];
+    }
+
+    fn and_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        self.v_reg[vx] &= self.v_reg[vy];
+    }
+
+    fn xor_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        self.v_reg[vx] ^= self.v_reg[vy];
+    }
+
+    fn add_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        let (new_vx_reg, carry) = self.v_reg[vx].overflowing_add(self.v_reg[vy]);
+        let new_vf_reg = if carry { 1 } else { 0 };
+        self.v_reg[vx] = new_vx_reg;
+        self.v_reg[0xf] = new_vf_reg;
+    }
+
+    fn sub_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        let (new_vx_reg, carry) = self.v_reg[vx].overflowing_sub(self.v_reg[vy]);
+        let new_vf_reg = if carry { 0 } else { 1 };
+        self.v_reg[vx] = new_vx_reg;
+        self.v_reg[0xf] = new_vf_reg;
+    }
+
+    fn shr_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        self.v_reg[vx] >>= 1;
+        self.v_reg[0xf] = self.v_reg[vx] & 1;
+    }
+
+    fn subn_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        let (new_vx_reg, carry) = self.v_reg[vy].overflowing_sub(self.v_reg[vx]);
+        let new_vf_reg = if carry { 0 } else { 1 };
+        self.v_reg[vx] = new_vx_reg;
+        self.v_reg[0xf] = new_vf_reg;
+    }
+
+    fn shl_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        self.v_reg[vx] <<= 1;
+        self.v_reg[0xf] = (self.v_reg[vx] >> 7) & 1;
+    }
+
+    fn sne_vx_vy(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let vy = ((opcode & 0x00f0) >> 4) as usize;
+        if self.v_reg[vx] != self.v_reg[vy] {
+            self.pc += 2;
+        }
+    }
+
+    fn ld_i(&mut self, opcode: u16) {
+        self.i_reg = opcode & 0x0fff;
+    }
+
+    fn jump_v0(&mut self, opcode: u16) {
+        let nnn = opcode & 0x0fff;
+        self.pc = (self.v_reg[0] as u16) + nnn;
+    }
+
+    fn rnd_vx(&mut self, opcode: u16) {
+        let vx = ((opcode & 0x0f00) >> 8) as usize;
+        let byte = (opcode & 0x00ff) as u8;
+        let rng: u8 = random();
+        self.v_reg[vx] = rng & byte;
     }
 }
