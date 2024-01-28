e8f530e3f8175749c364a2cc1fc4608a1cc4d858
SamirArioui <arioui.samir@gmail.com>
2024-01-28 01:52:33 +0100

    feat: inialize sdl2 window into main.rs

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,21 +1,26 @@
-use chip8_core::Chip8;
+use chip8_core::*;
+use std::env;
+
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 
 fn main() {
-    let path = String::from("data/TETRIS");
-    let offset = 0x200;
-    let mut chip8 = Chip8::new(offset);
-    chip8.load_program(path);
-    loop {
-        println!("Continue(y/n):");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Invalide input");
-        let input = input.trim();
-        let opcode = chip8.get_curr_opcode();
-        println!("Current opcode: {:X}", opcode);
-        if input == "y" {
-            chip8.run_opcode(opcode);
-        } else {
-            break;
-        };
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage cargo run path/to/game");
     }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
 }
