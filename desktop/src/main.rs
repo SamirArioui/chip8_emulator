f9d824c29a0aac9d008c668cc14c4b3c0c869458
sarioui <arioui.samir@gmail.com>
2024-01-30 23:12:36 +0100

    feat: add tick loop to main

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,9 +1,17 @@
 use chip8_core::*;
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 20;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -23,4 +31,28 @@ fn main() {
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
     canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Chip8::new();
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+        }
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.tick_timers();
+    }
 }
+
